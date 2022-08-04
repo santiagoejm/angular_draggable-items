3b7a5b192f5ee4024087ac621bc66d1a8daab24f
santiago julian monsalve <s.julian.monsalve@accenture.com>
2022-08-04 12:26:36 +0200

    installing material cdk, bug draging to be fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './pages/main-view/main-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { MainViewComponent } from './pages/main-view/main-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
